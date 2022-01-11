125860f1565a4bc5c41b7831c5dd7dac719eb838
coelhovinicius <coelhovinicius@yahoo.com.br>
2022-01-11 19:17:59 -0300

    Func e Lambda - Exercicio

---
+++ Aula225.1_Exercicio/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Exercicio 
+ * 
+ * Fazer um programa que, a partir de uma lista de produtos, calcule a soma dos preços somente dos produtos 
+ * cujoS nomeS comecem com "T". 
+ */
+
+/* >>>PROGRAMA PRINCIPAL <<< */
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Aula225._1_Exercicio.Services;
+using Aula225._1_Exercicio.Entities;
+
+namespace Aula225._1_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Cria lista Product e preenche
+            List<Product> list = new List<Product>(); 
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            //Instancia variavel "ps" tipo "ProductService"
+            ProductService ps = new ProductService(); // Instancia "ps" do tipo "ProductService", sem argumentos 
+
+            /* Executa a funcao "FilteredSum", recebendo os argumentos "list" (lista de produdos), "p", tipo "Product"
+             * (o tipo e automaticamente detectado e incutido pelo C#), onde a letra do nome do produto seja na posicao
+             * 0 seja a letra "T" */
+            double sum = ps.FilteredSum(list, p => p.Name[0] == 'T');
+
+            Console.WriteLine("Sum = " + sum.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Aula225.1_Exercicio/Services/ProductService.cs
@@ -0,0 +1,26 @@
+﻿/*>>> CLASSE PRODUCTSERVICE - SERVICE <<< */
+using System;
+using System.Collections.Generic;
+using Aula225._1_Exercicio.Entities;
+
+namespace Aula225._1_Exercicio.Services
+{
+    class ProductService
+    {
+        /* Cria uma funcao "FilteredSum", recebendo como argumentos uma List ("list") do tipo "Product" e uma Func.
+         * Esta Func, por sua vez, recebe um elemento tipo "Product" como entrada e retorna um "bool" como saida. 
+         * O nome "criteria" foi dado a essa Func. */
+        public double FilteredSum(List<Product> list, Func<Product, bool> criteria) 
+        {
+            double sum = 0.0; // Variavel auxiliar
+            foreach (Product p in list) // Para cada produto da lista 
+            {
+                if (criteria.Invoke(p)) // Invoca as informacoes do produto
+                {
+                    sum += p.Price; // Soma os produtos
+                }
+            }
+            return sum; // Retorna a soma
+        }
+    }
+}
